dbd87ea62a261a461feae26fce1ae8a2eeb8ea65
jinxin <jinxin70@gmail.com>
2021-02-08 21:51:07 +0800

    修改发布脚本，首次克隆项目，publish目录不存在，从远程克隆下来

--- publish.sh
+++ publish.sh
@@ -1,15 +1,27 @@
-echo "========= hexo generate =========="
+if [ ! -d "./publish/" ]; then
+    echo "========= Publish folder does't exist, clone from remote now =========="
+    
+    git clone git@github.com:jameingh/jameingh.github.io.git publish
+
+    echo "========= Clone publish files complete =========="
+    
+    else
+
+    echo " Publish folder already exists"
+fi
+
+echo "========= Hexo generate =========="
 
 ## && 只有左边的执行成功才能执行右边的
 hexo clean && hexo g && \cp -rf public/ publish/ 
 
 # rm -rf publish/*
 
-echo "========= hexo generate complete ========="
+echo "========= Hexo generate complete ========="
 
 cd publish
 
-echo "========= git commit start ========="
+echo "========= Git commit start ========="
 
 # git init 
 
@@ -17,12 +29,12 @@ git add .
 
 git commit -m 'add files' 
 
-echo "========= git commit compelete ========="
+echo "========= Git commit compelete ========="
 
 # git remote add origin git@github.com:jameingh/jameingh.github.io.git 
 
-echo "========= git push start ========="
+echo "========= Git push start ========="
 
 git push -u origin main
 
-echo "========= git push compelete ========="
+echo "========= Git push compelete ========="
